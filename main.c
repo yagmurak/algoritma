10d15f639256061e7d76a10404aa8be17c94d866
yagmurak <100042431+yagmurak@users.noreply.github.com>
2022-03-21 22:32:36 +0300

    Add files via upload
    kosinüs algoritması

--- main.c
+++ main.c
@@ -1,28 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+main(){
+    float  i , a ,b , c;
+    int faktöriyel = 1;
+    int x = 3, n;
 
-int main(){
-    double  i , a , b ;
-    double faktoriyel = 1;
-    int x , n;
-    
-    printf("x degerini giriniz");
-    scanf("%d", &x);
-
-   
-    for (n = 1; n <= 50; n++)
-    {
-        faktoriyel *= (2*n) * (2*n + 1);
-        printf ("faktoriyel = %f\n", faktoriyel);
-        a = pow ((-1), n); // a = -1;
-        printf("a = %f\n",a);
-        b = pow (x, (2*n+1)); // b = 33;
-        printf("b = %f\n",b);
-        i += (a * b) / (double)faktoriyel;
-        
-    }
-    printf("\n işlem sonucu = %f", i+x);
-
-    return 0;
+        for (n = 1 ; n <= 5 ; n++)
+        {
+            a = pow ((-1) , n);
+            printf("a = %f\n",a);
+            b = pow (x ,(2*n));
+            printf("b = %f\n",b);
+            i += a * b/ 2*faktöriyel;
+            printf("%d. adım sonucu = %f",n,i);
+            printf("\n");
+        }
+    for ( c=1;c<= n;c++){
+        faktöriyel *= c;
+        printf ("faktöriyel = %d\n", faktöriyel);}
+        printf("\n işlem sonucu = %f",i);
 }
